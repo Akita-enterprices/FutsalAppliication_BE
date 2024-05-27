ce13a423c788648d87f26d240c69ce22743c275e
Fathima Zahra <zahraazaam07@gmail.com>
2024-05-27 22:36:45 +0530

    authroutes

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -1,9 +1,11 @@
 const express = require("express");
-const { signup, login } = require("../controllers/authController");
+const { signup, login, forgotPassword,reserPassword} = require("../controllers/authController");
 
 const router = express.Router();
 
 router.post("/signup", signup);
 router.post("/login", login);
+router.post("/forgot-password",forgotPassword);
+router.post("/reset-password",reserPassword);
 
 module.exports = router;
