95d07a4b522c4eb6f2f415a40a20377bef0bd550
Shahla UCA <shahla@unicornconnectedapps.com>
2025-03-18 21:46:11 +0530

    user mail configuration

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -7,20 +7,19 @@ const {
   getUserById,
   updateUser,
   deleteUser,
+  verifyEmail
 } = require("../controllers/authController");
 const verifyToken = require("../middleware/auth");
 
 const router = express.Router();
 
-// Authentication Routes
 router.post("/signup", signup);
 router.post("/userLogin", userLogin);
 router.get("/me", verifyToken, me);
-
-// User Management Routes
 router.get("/getAllUsers", getAllUsers);
 router.get("/users/:id", getUserById);
 router.put("/updateUser",updateUser);
 router.delete("/users/:id",deleteUser);
+router.get("/verify-email", verifyEmail);
 
 module.exports = router;
